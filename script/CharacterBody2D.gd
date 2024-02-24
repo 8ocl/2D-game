8f845840b67279a18a2271f007cd63a5e0f95f89
8ocl <86467582+8ocl@users.noreply.github.com>
2024-02-23 22:15:17 -0600

    animation added yuh

--- script/CharacterBody2D.gd
+++ script/CharacterBody2D.gd
@@ -1,28 +1,43 @@
 extends CharacterBody2D
+@onready var animated_sprite_2d = $AnimatedSprite2D
+@onready var ray_cast_2d = $RayCast2D
 
 
-const SPEED = 300.0
+const SPEED = 150
 const JUMP_VELOCITY = -400.0
+var animation_playing = false
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 
 func _physics_process(delta):
-	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("up") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("left", "right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+	
+	
+	# animation
+	if velocity.x < 0:
+		animated_sprite_2d.play("left_walk")
+		ray_cast_2d.rotation = 90
+		animation_playing = true
+	elif velocity.x > 0:
+		animated_sprite_2d.play("right_walk")
+		ray_cast_2d.rotation = -90
+		animation_playing = true
+	else:
+		animation_playing = false
+	
+	if ray_cast_2d.rotation == -90 && animation_playing == false:
+		animated_sprite_2d.play("right_idle")
+	elif ray_cast_2d.rotation == 90 && animation_playing == false:
+		animated_sprite_2d.play("left_idle")
 
 	move_and_slide()
