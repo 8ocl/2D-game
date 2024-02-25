eec5ff1ad9a81f46270b701f7888c5d792539625
8ocl <86467582+8ocl@users.noreply.github.com>
2024-02-24 20:42:24 -0600

    Revert "platttformms!!!"
    This reverts commit 740042040ef12ba50f7e1d5a7a71dbcf296c8359.

--- script/player.gd
+++ script/player.gd
@@ -1,19 +1,14 @@
 extends CharacterBody2D
 @onready var animated_sprite_2d = $AnimatedSprite2D
-@onready var looking_dir = $looking_dir
-@onready var coyotetimer = $coyotetimer
-
+@onready var ray_cast_2d = $RayCast2D
 
 var walking = false
 var idle = false
 var jumping = false
-var gravity_force = 3
+var gravity_force = 0.68
 
-var coyote = 2
-var can_jump = true
-
-const SPEED = 700
-const JUMP_VELOCITY = -1200
+const SPEED = 150.0
+const JUMP_VELOCITY = -270.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -23,15 +18,9 @@ func _physics_process(delta):
 	if not is_on_floor():
 		velocity.y += gravity * delta * gravity_force
 
-
-	if is_on_floor() && can_jump == false:
-		can_jump = true
-	elif can_jump == true && $coyotetimer.is_stopped():
-		$coyotetimer.start(coyote)
-
-	if can_jump == true:
-		if Input.is_action_just_pressed("ui_accept"):
-			velocity.y = JUMP_VELOCITY
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+		
 
 
 	var direction = Input.get_axis("left", "right")
@@ -45,37 +34,37 @@ func _physics_process(delta):
 	if velocity.x < 0:
 		animated_sprite_2d.play("walk")
 		animated_sprite_2d.flip_h = true
-		looking_dir.rotation = 90
+		ray_cast_2d.rotation = 90
 		walking = true
 	elif velocity.x > 0:
 		animated_sprite_2d.play("walk")
 		animated_sprite_2d.flip_h = false
-		looking_dir.rotation = -90
+		ray_cast_2d.rotation = -90
 		walking = true
 	else:
 		walking = false
-		
 	# IDLE ANIMATION
-	if looking_dir.rotation == -90 && walking == false:
+	if ray_cast_2d.rotation == -90 && walking == false:
 		animated_sprite_2d.play("idle")
 		animated_sprite_2d.flip_h = false
 		idle = true
-	elif looking_dir.rotation == 90 && walking == false:
+	elif ray_cast_2d.rotation == 90 && walking == false:
 		animated_sprite_2d.play("idle")
 		animated_sprite_2d.flip_h = true
 		idle = true
-		
 	# JUMP ANIMATION
-	if !is_on_floor() && looking_dir.rotation == -90:
+	if !is_on_floor() && ray_cast_2d.rotation == -90:
 		animated_sprite_2d.play("jump")
 		animated_sprite_2d.flip_h = false
 		jumping = true
-	elif !is_on_floor() && looking_dir.rotation == 90:
+	elif !is_on_floor() && ray_cast_2d.rotation == 90:
 		animated_sprite_2d.play("jump")
 		animated_sprite_2d.flip_h = true
 		jumping = true
 
 	move_and_slide()
 
-func _on_coyotetimer_timeout():
-	can_jump = false
+	
+	
+	
+	
